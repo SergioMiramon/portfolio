7627963fd2623d09acc1885d1a4ae0024f2d9497
Sergio Miramon <smiramon8@gmail.com>
2023-07-26 08:46:38 +0200

    refactor project

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -16,7 +16,7 @@ import "./Navbar.css";
 // };
 export const Navbar = () => `
 <nav>
-<a href="#" id="homelink"><img class="logo" src="./icons/images/all/logo.png"></a>
+<a href="#" id="homelink"><img class="logo" src="/icons/images/all/logo.png"></a>
 <ul>
 <li>
 <a href="#" id="projectslink">Projects</a>
