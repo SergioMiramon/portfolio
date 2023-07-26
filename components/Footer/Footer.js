d978df9ecbbba5f089e8970949a9634de0033542
Sergio Miramon <smiramon8@gmail.com>
2023-07-26 12:11:31 +0200

    refactor project

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import "./Footer.css";
 import { Button } from "../Button/Button";
 export const Footer = () => `
-<h2>Contact Me</h2>
+<a href="/icons/images/all/cv.pdf" download="C.V. Sergio_Miramón.pdf" target="blank"><h2>CV</h2></a>
 <div>
 ${Button("github", "./icons/images/all/github.png", "GitHub", "https://github.com/SergioMiramon")}
 </div>
