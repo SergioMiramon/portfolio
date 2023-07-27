e155d074de73c41cfa3d8c3b97763066139ca537
Sergio Miramon <smiramon8@gmail.com>
2023-07-27 11:30:19 +0200

    refactor portfolio

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import "./Footer.css";
 import { Button } from "../Button/Button";
 export const Footer = () => `
-<a href="mailto:smiramon8@gmail.com"><h2>-> GET IN TOUCH <-</h2></a>
+<a class="email" href="mailto:smiramon8@gmail.com"><i class="arrows icon-arrow-right2"></i><h2>GET IN TOUCH</h2></a>
 <div>
 ${Button("github", "./icons/images/all/github.png", "GitHub", "https://github.com/SergioMiramon")}
 </div>
--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -6,10 +6,13 @@ export const Home = () => {
   main.innerHTML = `
 <section class="home">
       <div class="name">
-        <h2>SERGIO</h2>
-        <h1>MIRAMÓN</h1>
+        <h1>SERGIO MIRAMÓN</h1>
       </div>
-      <h3>FULLSTACK DEVELOPER</h3>
-      <p>aficionado a la <a class="namemusic">música</a>, a la <a class="namegallery">fotografía</a> y al <a class="nametv" href="https://miramon.notion.site/SERIES-SEEN-THIS-2023-7749f5e3165e428c8894963dbd7a1bb0?pvs=4" target="blank">séptimo arte</a></p>
+      <div class="profession">
+        <h3>FULLSTACK DEVELOPER</h3>
+      </div>
+      <div class="info">
+      <p>aficionado a la <a class="namemusic">música</a>, a la <a class="namegallery" href="https://photo-gallery-one-rouge.vercel.app/">fotografía</a> y al <a class="nametv" href="https://miramon.notion.site/SERIES-SEEN-THIS-2023-7749f5e3165e428c8894963dbd7a1bb0?pvs=4" target="blank">séptimo arte</a></p>
+      </div>
 </section>`;
 };
