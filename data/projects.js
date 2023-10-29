d4f454bab14b384684622a2b7f15cbf23797a503
Sergio Miramon <smiramon8@gmail.com>
2023-10-29 13:22:26 +0100

    refactor portfolio

--- data/projects.js
+++ data/projects.js
@@ -7,17 +7,17 @@ export const projects = [
     link: "https://photo-gallery-one-rouge.vercel.app/",
   },
   {
-    title: "Playlists",
-    image: "./icons/projectsimages/playlists.png",
-    tech: ["JS", "CSS3", "HTML5", "Vite"],
-    github: "https://github.com/SergioMiramon/playlists",
-    link: "https://playlists-xi.vercel.app/",
-  },
-  {
     title: "Albums of 2023",
     image: "./icons/projectsimages/albums2023.png",
     tech: ["REACT", "JS", "CSS3", "HTML5"],
     github: "https://github.com/SergioMiramon/albums2023",
     link: "https://albums2023.vercel.app/",
   },
+  {
+    title: "Playlists",
+    image: "./icons/projectsimages/playlists.png",
+    tech: ["JS", "CSS3", "HTML5", "Vite"],
+    github: "https://github.com/SergioMiramon/playlists",
+    link: "https://playlists-xi.vercel.app/",
+  }
 ];
